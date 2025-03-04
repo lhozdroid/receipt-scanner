7142269a2813fcff4cc20ebc8f1e8c7a8e18a34c
Luis Salas <lhoz@Lhoz-PC.ssnetwork>
2025-03-04 15:52:38 -0500

    Added default spring boot project.

---
+++ src/test/java/com/lavishlyholiday/invoicescanner/InvoiceScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavishlyholiday.invoicescanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
